6df5f23800eaad3f67a1a60f17ceef63b634fc7a
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-26 00:27:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ client.on("message", async message => {
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
-  if(command === "ping") {
+    if(message.content.startsWith(prefix + "ping")){
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");

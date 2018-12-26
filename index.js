e06bc9a39b956eacdb0eef7b13a709a65b624e14
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-26 21:12:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,110 @@
 const token = process.env.token;
-var discord = require('discord.js');
-var fs = require('fs');
-var randomColour = require('randomcolor'); // yes, the creator of this package does not speak the real english
-var Config = require('./config.json');
+//by revenge ! dont share this code in any discord server !
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const prefix = '?'
+//information about the bot
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[═════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations About Rainbow bot:')
+  console.log('')
+  console.log(`Servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`Channels! [ " ${client.channels.size} " ]`);
+  console.log(`Arch! [ " ${process.arch} " ]`);
+  console.log(`Platform! [ " ${process.platform} " ]`);
+  console.log(`Node Version! [ " ${process.version}" ]`);
+  console.log(`Prefix! [ " ${prefix}" ]`);
+  console.log(`Language! [ " NodeJS " ]`);
+  console.log(`Ram Usage! [ " ${(process.memoryUsage().rss / 1048576).toFixed()}MB " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(`${client.user.username} Is Online`)
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log('Created By: Revenge')
+  console.log('╚[════════════]╝')
+client.user.setActivity("r#help | By Revenge",{type: 'WATCHING'});
+console.log('Done The Watching Setup Completed')
+	
+});
+//Best Rainbow Bot .
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
+	  let role = message.guild.roles.find('name', 'Rainbow bot.')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow bot.",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
 
-class Bot {
-    constructor(){
-        this.servers = require('./servers.json');
-        this.discordClient = new discord.Client({sync: true});
-        
-        this.discordClient.on("ready", () => {this.initialize();});
-        
-        this.discordClient.on("message", (msg) => {this.processMessage(msg)});
-        
-        this.discordClient.login(Config.discord_token);
-    }
-    
-    initialize() {
-        this.log("Connected to discord.");
-        
-        setInterval(() => {
-            this.randomizeRoleColors();
-        }, Config.randomize_delay*1000);
-    }
-    
-    processMessage(msg) {
-        if(msg.content.startsWith(">addrole")) {
-            for(var role of msg.mentions.roles.array()) {
-                msg.reply("Added " + role + " to list of rainbow roles.");
-                
-                this.addRainbowRole(msg.guild.id, role.id);
-            }
-        }
-    }
-    
-    randomizeRoleColors() {
-        for(var server in this.servers) {
-            var liveGuild = this.discordClient.guilds.get(server);
-            
-            if (!liveGuild) {
-                this.error("Guild with ID " + server+ " no longer exists or the bot has been removed from it.");
-                continue;
-            }
-            
-            for(var role of this.servers[server]) {
-                var liveRole = liveGuild.roles.get(role);
-                
-                liveRole.setColor(randomColour(), "Rainbowbot random role color randomizer.");
-            }
-        }
-    }
-    
-    addRainbowRole(guild, role) {
-        if(this.servers[guild] == undefined) {
-            this.servers[guild] = [];
-        }
-        
-        for(var existingRole of this.servers[guild]) {
-            if(existingRole == role) {
-                return "That role has already been added.";
-            }
-        }
-        
-        this.servers[guild].push(role);
-        this.saveServers();
-    } 
-    
-    saveServers() {
-        fs.writeFileSync("./servers.json", JSON.stringify(this.servers), "utf8");
-        this.log("Saved servers file.");
-    }
-    
-    log(message) {
-        console.log("\x1b[32mINFO\x1b[37m - \x1b[0m" + message);
-    }
-    
-    error(message) {
-        console.log("\x1b[31mERROR\x1b[37m - \x1b[0m" + message);
-    }
 }
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
 
-var instance = new Bot();
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', 'Rainbow bot.');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 5000);//the rainbow time
+})
+
+client.on("message", message => {//new msg event
+  if (message.content === "?help") {//the help cmd
+      message.react('🌈')
+        let rainembed = new Discord.RichEmbed()//new embed
+        //the embed description (help msg)
+        .setDescription(`**
+=====================🌈 RainbowBot. 🌈=====================
+r#set 
+ - لإنشاء رتبة الرينبو وبدا الرينبو
+- To create the role of the Rainbow & Start The Rainbow
+r#inv 
+- لدعوة البوت
+- To Invite the bot
+خطوات لو الرتبة م أشتغلت .!!
+1- ضع رتبة الرينبو فوق الالوان أو الرتب الملونه لو فيه
+2- ضع رتبة البوت فوق رتبة الرينبو
+The steps of the role did not worked .!!
+1- Place the role of the Rainbow above the colors or colored ranks if it
+2- Put the bot role above the role of the Rainbow 
+رآبط البوت - Bot Invite link
+- hhttp://cutt.us/HPkx6
+=====================🌈 RainbowBot. 🌈=====================
+**`)
+message.author.sendEmbed(rainembed)//send the embed to the author dm
+    }})
+   client.on('message', message => {//new cmd
+	   if(message.content.startsWith(`r#inv`)) { //the invite bot cmd
+		   if(!message.channel.guild) return;
+                 message.react('🌈')
+		   var embed = new Discord.RichEmbed()
+		   .setTitle(">> ClickHere To Add" + `${client.user.username}` + " <<")
+		   .setURL("https://discordapp.com/oauth2/authorize?client_id=" + `${client.user.id}` + "&scope=bot&permissions=2080374975")
+		   .setTimestamp()
+		   .setFooter(`Requested By | ${message.author.username}`)
+		   .setColor("RANDOM")
+		   message.author.send({embed})//send the url in the author dm
+	   }
+   });
 client.login(token);

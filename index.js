c0112860c9a85b1cc779dfd8c2b09f75a78c46df
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-28 20:36:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -176,6 +176,9 @@ client.on("message", async message => {
   if(message.author.bot) return;
   
   if(message.content.indexOf(config.prefix) !== 0) return;
+	
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
     
     if(message.content.startsWith(prefix + "ping")){
     const m = await message.channel.send("Ping?");

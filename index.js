f609f0847f93307a8728d8ec9b175fddc62183a6
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-31 15:36:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -132,12 +132,36 @@ userAnswer = "";
 });
 
 client.on("message", async message => {
-  if(message.author.bot) return;
-  
-  if(message.content.indexOf(config.prefix) !== 0) return;
+ 
   
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+	
+	 msg = message.content.toLocaleLowerCase();
+
+           if (answered == false && message.author == quizUser) {
+               userAnswer = msg;
+               if (userAnswer == cAnswer) {
+                   message.reply ("Correct");
+               }
+               else{
+                   message.reply("Faux");
+               }
+               answered = true; cAnswer = ""; userAnswer = "";
+           }
+         
+           if (msg.startsWith(prefix + "quiz" )){
+               number = 3;
+               var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+               switch(random){
+                   case 1: message.channel.send ("Que ce qu'es le mot Tellurique: \n1 - Qualifie des planètes similaires a la Terre \n2 - Un minerai extrait de roche \n3 - Un mouvement spirituelle \n4 - Obi wan kenobi "); cAnswer = "1"; break;
+                   case 2: message.channel.send ("Quel est l'auteur de la citation « Je n’ai pas échoué. J’ai simplement trouvé 10 000 solutions qui ne fonctionnent pas. » ?  \n1 - Benjamin Franklin. \n2 - Albert Einstein. \n3 - Thomas Edison. \n4 - Emmanuel Macron."); cAnswer = "3"; break;
+                   case 3: message.channel.send ("Parmis ces anime quelle l'anime avec le plus d'épisodes ? \n1 - Detective Conan \n2 - One Piece \n3 - Doraemon \n4 - C'est l'anime le plus long"); cAnswer = "3"; break;
+         
+               }
+                answered = false;
+               quizUser = message.author;
+           }
     
   
 

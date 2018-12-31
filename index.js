2ecc612ed2019f0b123d09f65c4b11d805c0dd2f
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-31 14:53:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "?" ;
 
-client.on("ready",() => {
+client.on("message",  (message) => {
  
     
   answered = true;

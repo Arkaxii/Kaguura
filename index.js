6812c1899253586526508bfc01d9de6bfa89d487
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2018-12-31 14:29:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const prefix = "?" ;
 client.on("message", async message => {
   if(message.author.bot) return;
   
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
     
   answered = true;
   cAnswer = "";

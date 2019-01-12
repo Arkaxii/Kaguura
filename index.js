cebf4f434c1c6f488de44b742a747fcda4a260f1
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-12 23:04:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -984,6 +984,33 @@ if (randnum == 3){
                 answered = false;
                quizUser = message.author;
            }
+	
+           if (msg.startsWith(prefix + "im" )){
+    var userinf = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setThumbnail(message.author.avatarURL)
+        .setDescription("This is the user's info!")
+        .setColor(0x00FF00)
+        .addField("Full Username:", `${message.author.username}#${message.author.discriminator}`)
+        .addField("ID:", message.author.id)
+        .addField("Created At:", message.author.createdAt)
+
+        message.channel.send(userinf);
+
+  }else{
+
+    var userinfoo = new Discord.RichEmbed()
+        .setAuthor(memberInfo.displayName)
+        .setThumbnail(memberInfo.user.avatarURL)
+        .setDescription("This is the user's info!")
+        .setColor(0x00FF00)
+        .addField("Full Username:", `${memberInfo.user.username}#${memberInfo.user.discriminator}`)
+        .addField("ID:", memberInfo.id)
+        .addField("Created At:", memberInfo.user.createdAt)
+
+        message.channel.send(userinfoo);
+  }
+
   
 });
 client.login(token);

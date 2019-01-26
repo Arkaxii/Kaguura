cdc54b0050ced2dd95c7bfb4c3774847b68d7914
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-26 20:21:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,22 @@ client.on("message", async message => {
      message.channel.send(`${user.username} - Balance: $${balance}`);
      }
 
+     if(commande ==="daily"){
+       let cooldown = 8.64+7,
+          amount = 250;
+       let lastDaily = await db.fetch(`lastDaily_${message.author.id}`);
+       if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0){
+         let timeObj = ms(cooldown - (Date.now() - lastDaily));
+         message.channel.send(`Patiente encore **${timeObj.hour}h ${timeObj.minutes}m**!`);
+
+       }else{message.channel.send(`Tu as ressu $${amount}`);
+      db.set(`lastDaily_${message.author.id}`, Date.now());
+    db.add(`userBalance_${message.author.id}`, 250);
+         
+            }
+
+     }
+
 	 
 });
 client.login(token);

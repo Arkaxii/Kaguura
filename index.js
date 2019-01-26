95c178d188bc1ecbff28d815f89ed5843c261b31
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-26 21:12:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,23 @@ client.on("message", async message => {
            message.channel.send(`**Tu as donner $${amount} a ${target.user.tag}!**`)
 
      }
+	
+	if(command === "arkapay"){
+      if(message.author.id !== config.ownerID)            
+      return message.reply("Tu t'attendais a quoi? Cette commande est reserver à mon créateur");
+      let target = message.mentions.members.first();
+      if(!target)
+        return message.reply("Faut mentionner quelqu'un!");
+      let amount = parseInt(args.join('').replace(target, ''));
+       if(isNaN(amount)) 
+       return message.channel.send("Tu n'as pas mis de montant!");
+       let targetBalance = await db.fetch(`userBalance_${target.id}`),
+       if (targetBalance === null) targetBalance = 0;
+       db.add(`userBalance_${target.id}`,amount);
+       message.channel.send(`**Tu as donner $${amount} a ${target.user.tag}!**`)
+
+
+     }
 
 	 
 });

8ee56d0a952639d2c2a368d7e03e5b610442b67e
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-26 20:49:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,25 @@ client.on("message", async message => {
 
      }
 
+     if(command ==="pay"){
+       if(!message.mentions.members.first())
+       return message.channel.send("Mentionne un utilisateur!");
+       let target = message.mentions.members.first(),
+       amount = parseInt(args.join('').replace(target, ''));
+       if(isNaN(amount)) 
+       return message.channel.send("**Fait pas ton radin et défini un montant!**");
+       let targetBalance = await db.fetch(`userBalance_${targetMember.id}`),
+           selfBalance = await db.fetch(`userBalance_${message.author.id}`);
+           if (targetBalance === null) targetBalance = 0;
+           if (selfBalance === null) selfBalance = 0;
+           if (amount > selfBalance) 
+           return message.channel.send("**Ne donne pas plus que ce que tu as!**");
+           db.add(`userBalance_${targetMember.id}`,amount);
+           db.subtract(`userBalance_${message.author.id}`, amount);
+           message.channel.send(`**Tu as donner $${amount} a ${targetMember.user.tag}!**`)
+
+     }
+
 	 
 });
 client.login(token);

51e2041d5fcb45adfe0b3c8f512f3bcd768db4ff
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-31 12:02:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1371,7 +1371,7 @@ if(message.content.startsWith(prefix + "sondage")){
                                message.channel.send(reponse[result]);
             }
      
-          if(command === "shop"){
+            if(command === prefix +"shop"){
                 var shoop = new Discord.RichEmbed()
                 .setColor(0x00FF00)
                 .setTitle("Badge")

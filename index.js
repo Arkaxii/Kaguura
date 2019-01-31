280a4a882d3ab089d5bafab52d27af02f8528a17
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-31 20:36:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1270,31 +1270,31 @@ if (randnum == 3){
            }
 
 
-               if(message.content.startsWith(prefix + "infos")){
+if(message.content.startsWith(prefix + "infos")){
            let memberInfo = message.mentions.members.first();
 
            if(!memberInfo){
              var userinf = new Discord.RichEmbed()
-                 .setAuthor(message.author.username)
-                 .setThumbnail(message.author.avatarURL)
-                 .setDescription("This is the user's info!")
-                 .setColor(0x00FF00)
-                 .addField("Full Username:", `${message.author.username}#${message.author.discriminator}`)
-                 .addField("ID:", message.author.id)
-                 .addField("Created At:", message.author.createdAt)
+             .setAuthor(message.author.username)
+             .setThumbnail(message.author.avatarURL)
+             .setDescription("Voici les infos!")
+             .setColor(0x00FF00)
+             .addField("Pseudo Discord complet:", `${message.author.username}#${message.author.discriminator}`)
+             .addField("ID:", message.author.id)
+             .addField("Crée le:", message.author.createdAt)
          
                  message.channel.send(userinf);
          
            }else{
          
              var userinfoo = new Discord.RichEmbed()
-                 .setAuthor(memberInfo.displayName)
-                 .setThumbnail(memberInfo.user.avatarURL)
-                 .setDescription("This is the user's info!")
-                 .setColor(0x00FF00)
-                 .addField("Full Username:", `${memberInfo.user.username}#${memberInfo.user.discriminator}`)
-                 .addField("ID:", memberInfo.id)
-                 .addField("Created At:", memberInfo.user.createdAt)
+             .setAuthor(target.displayName)
+             .setThumbnail(target.user.avatarURL)
+             .setDescription("Voici les infos!")
+             .setColor(0x00FF00)
+             .addField("Pseudo Discord complet:", `${target.user.username}#${target.user.discriminator}`)
+             .addField("ID:", target.id)
+             .addField("Crée le:", target.user.createdAt)
          
                  message.channel.send(userinfoo);
            }

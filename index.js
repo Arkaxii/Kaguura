6d7256938139dccd0fed8f905456f0c965db094c
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-01-31 12:37:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -158,9 +158,13 @@ userAnswer = "";
 });
 
 client.on("message", async message => {
+	const config = require("./config.json");
+
  
+    if (message.content.indexOf(config.prefix) !== 0) return;
+
   
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 	
      msg = message.content.toLocaleLowerCase();

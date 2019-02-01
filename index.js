7a19ef12360e0b2bb9a54415ecab33da599042a0
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-02-01 13:24:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ if(message.content.startsWith(prefix + `mp`)) {
     .setTimestamp(sayMessage)
     .setFooter(`Requested By | ${message.author.username}`)
     .setColor("RANDOM")
-    message.ownerID.reply({embed})
+    message.author.send({embed})
 }
    });
 

3ba05c83b47a3faaf87fec01234def5c7116e784
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-05-09 23:17:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2332,26 +2332,7 @@ if(command ==="cancel"){
         }
 
 	
-        if (command === "tr") {
-            if (args.length < 3) {
-                message.reply(speech.BOT_TRANS_SPECIFIC_ERROR);
-            } else {
-                let argFrom = args[0].toLowerCase();
-                let argTo = args[1].toLowerCase();
-    
-                let lang_from = language.filter(ele => ele.name === argFrom)[0].abrv;
-                let lang_to = language.filter(ele => ele.name=== argTo)[0].abrv;
-                let text = args.slice(2).join(' ');
-    
-                translate(text, {from: lang_from, to: lang_to})
-                    .then(res => message.channel.send(res.text))
-                    .catch(err => console.log(speech.BOT_TRANSLATION_ERROR + err));
-            }
-        }
-
-	
-	
-	
+      
 	
     });
     

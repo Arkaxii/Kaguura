90f2398a6e412c3007d559c53c3082e9c57472ab
Arkaxii <45296939+Arkaxii@users.noreply.github.com>
2019-05-10 11:30:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -547,7 +547,28 @@ if(command ==="cancel"){
      message.channel.send(`${user.username} - Balance: $${balance}`);
      }
 
-     if(command ==="daily"){
+
+if(commmand ==="daily"){
+
+     let timeout = 86400000 // 24 hours in milliseconds, change if you'd like.
+     let amount = 500
+     // random amount: Math.floor(Math.random() * 1000) + 1;
+     let daily = await db.fetch(`daily_${message.author.id}`);
+     if (daily !== null && timeout - (Date.now() - daily) > 0) {
+         let time = ms(timeout - (Date.now() - daily));
+         message.channel.send(`You already collected ur daily reward, you can come back and collect it in **${time.hours}h ${time.minutes}m ${time.seconds}s**!`)
+     } else {
+     let embed = new Discord.RichEmbed()
+     .setAuthor(`Daily`, message.author.displayAvatarURL)
+     .setColor("GREEN")
+     .setDescription(`**Daily Reward**`)
+     .addField(`Collected`, amount)
+     message.channel.send(embed)
+     db.add(`money_${message.author.id}`, amount)
+     db.set(`daily_${message.author.id}`, Date.now()) 
+     }
+ }
+     /*   if(command ==="daily"){
        let cooldown = 8.64e+7,
           amount = 250;
        let lastDaily = await db.fetch(`lastDaily_${message.author.id}`);
@@ -561,8 +582,7 @@ if(command ==="cancel"){
 
             }
 
-     }
-
+     }*/
      if(command ==="pay"){
        if(!message.mentions.members.first())
        return message.channel.send("Mentionne un utilisateur!");

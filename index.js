fa3c8e07c35db08ab007b83575286cd2effa2847
Arkaxii <lucas51120@hotmail.com>
2019-05-26 16:34:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1598,7 +1598,7 @@ if(command ==="cancel"){
 
                 client.on('messageReactionAdd', (reaction, user) =>{
                 
-                    if (reaction.emoji.name === "⏩" && message.user.id !== client.author.id) {
+                    if (reaction.emoji.name === "⏩" && user.id !== client.author.id) {
                         message.delete();
                         
                         var p2 = new Discord.RichEmbed()

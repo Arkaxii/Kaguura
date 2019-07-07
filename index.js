fad186297228c3287746e89bada466134c9638b1
Arkaxii <lucas51120@hotmail.com>
2019-07-07 19:26:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -436,8 +436,8 @@ client.on("ready", () => {
                 var random = Math.floor (Math.random() * (number - 1 + 1)) + 1;
                 switch(random){
                
-                    case 1: message.channel.send ("c'est la ref 1"); cAnswer = "ref 1 test";if(!userAnswer){ setTimeout(message.delet() , 5000)};break; 
-                    case 2: message.channel.send ("c'est la ref 2"); cAnswer = "ref 2 test";if(!userAnswer){ setTimeout(message.delet() , 5000)};break; 
+                    case 1: message.channel.send ("c'est la ref 1"); cAnswer = "ref 1 test";if(!userAnswer){message.delet(5000)};break; 
+                    case 2: message.channel.send ("c'est la ref 2"); cAnswer = "ref 2 test";if(!userAnswer){message.delet(5000)};break; 
             
             
                 }

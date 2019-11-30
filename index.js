1806a73f73c4e2f5ca9e285992911e183c0b2758
Arkaxii <lucas51120@hotmail.com>
2019-11-30 23:45:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2154,7 +2154,7 @@ var kanaa = ka[Math.floor(Math.random() * ka.length)] ;
 
 
                 if(command === "test"){
-                    message.channel.send("Test = ``" + message.guild.members.username.random()+"`` ");
+                    message.channel.send("Test = ``" + message.guild.username.random()+"`` ");
                 };
 
 

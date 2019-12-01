69cbeb8bc787872a84eb1b13f2168e6bb643a12b
Arkaxii <lucas51120@hotmail.com>
2019-12-01 13:31:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2165,7 +2165,7 @@ var kanaa = ka[Math.floor(Math.random() * ka.length)] ;
 
 
 if(command === "love"){
-    var l = message.channel.send(message.author.username)
+    var l = await message.channel.send(message.author.username)
     l.edit(message.author.username+"==25%=<3==>"+message.guild.members.random().displayName)
     l.edit(message.author.username+"==50%=<3==>"+message.guild.members.random().displayName)
     l.edit(message.author.username+"==75%=<3==>"+message.guild.members.random().displayName)

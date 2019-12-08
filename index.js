4cab5e20e615bae275948df70ddaac835cef3f4a
Arkaxii <lucas51120@hotmail.com>
2019-12-08 01:11:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2124,7 +2124,7 @@ message.channel.send(embetest).then(message =>{
         const forwards = message.createReactionCollector(forwardsFilter, {timer: 6000});
 
         backwards.on('collect', r => {
-            r.remove(r.users.filter(u => u === message.author).first());
+            r.remove(r.users.filter(u => !u.bot).first());
 
             if (page ===1) return;
             page--;
@@ -2135,7 +2135,7 @@ message.channel.send(embetest).then(message =>{
 })
 
 forwards.on('collect', r => {
-    r.remove(r.users.filter(u => u === message.author).first());
+    r.remove(r.users.filter(u => !u.bot).first());
 
     if (page === pages.length) return;
     page++;

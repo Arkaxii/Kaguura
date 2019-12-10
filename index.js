38318da3497e932a2561ac417369d26fcda1b641
Arkaxii <lucas51120@hotmail.com>
2019-12-10 01:08:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1313,6 +1313,97 @@ if(!rolerain)
    
 
 
+
+
+
+
+
+
+
+
+        {
+            function random(min, max){
+               min = Math.ceil(1);
+               max = Math.floor(6);
+               randnum = Math.floor(Math.random() * (max - min +1) +min);
+           }
+            if(command ==="kick_roulette"){
+               
+               if (randnum == 1){
+                const loose =  message.reply("Charge le pistolet et")
+                loose.edit("Charge le pistolet et.")
+                loose.edit("Charge le pistolet et..")
+                loose.edit("Charge le pistolet et...")
+                loose.edit("Charge le pistolet et...PAN! ")
+                message.channel.send("Ah désolé "+ author.username +" mais tu est kick du serveur! ")
+                await author.kick("A perdu à la roulette du kick ")
+               }
+
+               if (randnum == 2){
+                  const win = message.reply("Charge le pistolet et")
+                  win.edit("Charge le pistolet et.")
+                  win.edit("Charge le pistolet et..")
+                  win.edit("Charge le pistolet et...")
+                  win.edit("Charge le pistolet et...Clik! GG Tu gagne le pouvoir de refaire une roulette jusqua que tu perde!  ")
+               }
+               if (randnum == 3){
+                const win1 = message.reply("Charge le pistolet et")
+                win1.edit("Charge le pistolet et.")
+                win1.edit("Charge le pistolet et..")
+                win1.edit("Charge le pistolet et...")
+                win1.edit("Charge le pistolet et...Clik! GG Tu gagne le pouvoir de refaire une roulette jusqua que tu perde!  ")
+             }
+             if (randnum == 4){
+                const win2 = message.reply("Charge le pistolet et")
+                win2.edit("Charge le pistolet et.")
+                win2.edit("Charge le pistolet et..")
+                win2.edit("Charge le pistolet et...")
+                win2.edit("Charge le pistolet et...Clik! GG Tu gagne le pouvoir de refaire une roulette jusqua que tu perde!  ")
+             }
+             if (randnum == 5){
+                const win3 = message.reply("Charge le pistolet et")
+                win3.edit("Charge le pistolet et.")
+                win3.edit("Charge le pistolet et..")
+                win3.edit("Charge le pistolet et...")
+                win3.edit("Charge le pistolet et...Clik! GG Tu gagne le pouvoir de refaire une roulette jusqua que tu perde!  ")
+             }
+             if (randnum == 6){
+                const win4 = message.reply("Charge le pistolet et")
+                win4.edit("Charge le pistolet et.")
+                win4.edit("Charge le pistolet et..")
+                win4.edit("Charge le pistolet et...")
+                win4.edit("Charge le pistolet et...Clik! GG Tu gagne le pouvoir de refaire une roulette jusqua que tu perde!  ")
+             }
+
+
+
+
+
+            }}
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if(command === "monstre") {
             if(message.author.id == config.ownerID)     
 

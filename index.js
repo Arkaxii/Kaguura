f0053fa429de86d7b9aa435bdb2c501db7516332
Arkaxii <lucas51120@hotmail.com>
2019-12-16 15:12:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3129,14 +3129,14 @@ message.channel.send(eith)
 function randomxp1(min, max){
     min = Math.ceil(0);
     max = Math.floor(5);
-    randxp1 = Math.floor(Math.randomxp1() * (max - min +1) +min);
+   const randxp1 = Math.floor(Math.randomxp1() * (max - min +1) +min);
     }
 
 
 function random(min, max){
     min = Math.ceil(1);
     max = Math.floor(10);
-    randwin = Math.floor(Math.random() * (max - min +1) +min);
+   const randwin = Math.floor(Math.random() * (max - min +1) +min);
     }
 
 if(command === "tower"){

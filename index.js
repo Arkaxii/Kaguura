c631549077e04556e2e6c0b1d38efcd6dfd18e78
Arkaxii <lucas51120@hotmail.com>
2019-12-19 19:41:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1535,6 +1535,57 @@ if(!rolerain)
 
 
        
+        if (command ==="mute") {
+            if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+            let member = message.mentions.members.first()
+            if (!member) return message.channel.send("Membre introuvable")
+            if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
+            if (!member.manageable) return message.channel.send("Je ne peux pas mute ce membre")
+            let muterole = message.guild.roles.find(role => role.name === 'Muted')
+            if (muterole) {
+                member.addRole(muterole)
+                message.channel.send("Tu as été mute pour 1h")
+            }
+            else {
+                message.guild.createRole({name: 'Muted', permissions: 0}).then(function (role) {
+                    message.guild.channels.filter(channel => channel.type === 'text').forEach(function (channel) {
+                        channel.overwritePermissions(role, {
+                            SEND_MESSAGES: false
+                        })
+                    })
+                    member.addRole(role)
+                    message.channel.send(member + ' a été mute :white_check_mark:')
+                })
+            }
+            setTimout(() => {
+                member.removeRole(mutedRole, `tu peut reparler`);
+              }, minutes * 3600000); 
+        }
+
+
+
+if(command ==="t2"){
+
+let mutrole = message.guild.roles.find( 'name' , 'MUTE' )
+if(!mutrole)
+    guild.createRole({
+        name: 'MUTE',
+        color: 'RED',
+      })
+    
+    
+      let mutedRole = message.guild.find(role => role.name == "Mute");
+  // This is the member you want to mute
+  let member = message.mentions.members.first();
+
+  // Mute the user
+  member.addRole(mutedRole, ` Mute pour ${minutes} minutes. Raisons : <<Les morts ne parle pas.>>`);
+
+  // Unmute them after x minutes
+  setTimout(() => {
+    member.removeRole(mutedRole, `Temporary mute expired.`);
+  }, minutes * 3600000); 
+}
 
 
 
